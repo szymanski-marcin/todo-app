0e8e53fa5f5ff29ea147151b18a1b20928ecac96
Marcin Szymański <107401426+szymanski-marcin@users.noreply.github.com>
2024-02-04 17:00:10 +0100

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,179 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from 'react';
+import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import * as TodosApiCommands from './api/todos';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Errors } from './types/Errors';
+import { FilterValue } from './types/FilterValue';
+import { ErrorNotification } from './components/ErrorNotification';
 
-function App() {
+const USER_ID = 12084;
+
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filterValue, setFilterValue] = useState(FilterValue.All);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [errorMessage, setErrorMessage] = useState<Errors>(Errors.Null);
+  const [loadingTodoId, setLoadingTodoId] = useState<number[]>([]);
+
+  const handleError = (error: Errors) => {
+    setErrorMessage(error);
+  };
+
+  useEffect(
+    () => {
+      TodosApiCommands.getTodos(USER_ID)
+        .then(setTodos)
+        .catch(() => handleError(Errors.LoadTodos));
+    }, [],
+  );
+
+  const todosToRender = useMemo(
+    () => {
+      return todos.filter(todo => {
+        return filterValue === FilterValue.All
+          || (filterValue === FilterValue.Completed
+            ? todo.completed : !todo.completed);
+      });
+    },
+    [todos, filterValue],
+  );
+
+  const addTodo = (inputValue: string) => {
+    const data = {
+      id: 0,
+      title: inputValue,
+      completed: false,
+      userId: USER_ID,
+    };
+
+    setTempTodo(data);
+    setLoadingTodoId([0]);
+
+    TodosApiCommands.createTodo(data)
+      .then((createdTodo) => {
+        setTodos((currentTodos) => [...currentTodos, createdTodo]);
+      })
+      .catch(() => {
+        handleError(Errors.AddTodo);
+      })
+      .finally(() => {
+        setTempTodo(null);
+        setLoadingTodoId([]);
+      });
+  };
+
+  const updateTodo = (todoToUpdate: Todo): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      setLoadingTodoId(loadingTodos => [...loadingTodos, todoToUpdate.id]);
+      TodosApiCommands
+        .updateTodo(todoToUpdate.id, todoToUpdate)
+        .then(updatedTodo => {
+          setTodos(currentTodos => {
+            return currentTodos
+              .map(todo => (todo.id === todoToUpdate.id ? updatedTodo : todo));
+          });
+          resolve();
+        })
+        .catch(() => {
+          handleError(Errors.UpdateTodo);
+          reject();
+        })
+        .finally(() => {
+          setLoadingTodoId([]);
+        });
+    });
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setLoadingTodoId(currentLoadingTodoId => [...currentLoadingTodoId, todoId]);
+    TodosApiCommands.deleteTodo(todoId)
+      .then(() => setTodos(
+        currentTodos => currentTodos.filter(todo => todo.id !== todoId),
+      ))
+      .catch(() => handleError(Errors.DeleteTodo))
+      .finally(() => {
+        setLoadingTodoId([]);
+      });
+  };
+
+  const deleteAllCompletedTodos = (todoId: number) => {
+    const completedTodos = todos.filter(todo => todo.completed);
+
+    setLoadingTodoId(completedTodos.map(todo => todo.id));
+    TodosApiCommands.deleteTodo(todoId)
+      .then(() => setTodos(
+        currentTodos => currentTodos.filter(todo => todo.id !== todoId),
+      ))
+      .catch(() => handleError(Errors.DeleteTodo))
+      .finally(() => {
+        setLoadingTodoId(prevIds => prevIds.filter(id => id !== todoId));
+      });
+  };
+
+  const handleEditTodo = (todoId: number, newTitle: string) => {
+    if (!newTitle) {
+      return deleteTodo(todoId);
+    }
+
+    setLoadingTodoId(prevIds => [...prevIds, todoId]);
+
+    return TodosApiCommands.updateTodoTitle(todoId, newTitle)
+      .then(updatedTodo => {
+        setTodos(currentTodos => currentTodos.map(
+          todo => (todoId === todo.id ? updatedTodo : todo),
+        ));
+      })
+      .catch(() => {
+        setErrorMessage(Errors.EmptyTitle);
+      })
+      .finally(() => {
+        setLoadingTodoId(prevIds => prevIds.filter(id => id !== todoId));
+      });
+  };
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          updateTodo={updateTodo}
+          todos={todos}
+          addTodo={addTodo}
+          handleError={handleError}
+          disabled={loadingTodoId.length > 0}
+        />
+
+        <TodoList
+          todos={todosToRender}
+          tempTodo={tempTodo}
+          deleteTodo={deleteTodo}
+          loadingTodoId={loadingTodoId}
+          updateTodo={updateTodo}
+          handleEditTodo={handleEditTodo}
+        />
+
+        {todos.length > 0 && (
+          <Footer
+            todos={todosToRender}
+            setFilterValue={setFilterValue}
+            filterValue={filterValue}
+            deleteTodo={deleteAllCompletedTodos}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
     </div>
   );
-}
-
-export default App;
+};
